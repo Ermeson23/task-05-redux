a3f2359efe3f7fd03aeb0c3ce33fbe9d560cf14f
vivalaclara <claramaestri07@gmail.com>
2024-02-19 02:37:27 -0300

    added navigate

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useLoginMutation } from '../../store/api/reqres';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
-import cat from '../../assets/logincat.webp'
-import '../auth.css' //styles for login and register
+import cat from '../../assets/logincat.webp';
+import '../auth.css'; //styles for login and register
 import { Link } from 'react-router-dom';
-
 
 interface LoginFormProps {}
 
@@ -16,6 +16,7 @@ interface FormValues {
 
 const LoginForm: React.FC<LoginFormProps> = () => {
   const [login, { isLoading }] = useLoginMutation();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,8 +26,9 @@ const LoginForm: React.FC<LoginFormProps> = () => {
     };
 
     try {
-      await login(formData); 
+      await login(formData);
       console.log('Logged in successfully');
+      navigate('/books');
     } catch (error) {
       console.error('Failed to login:', error);
       alert('Failed to login');
@@ -35,33 +37,32 @@ const LoginForm: React.FC<LoginFormProps> = () => {
 
   return (
     <>
-     <Header/>
-     <main className='render'>
-      <section className='form-section'>
-        <div className='form-div'>
-        <div className='form'>
-          <div className='image'>
-        <img src={cat} alt='' loading='lazy'/>
-        </div>
-        <form onSubmit={handleSubmit} className='auth'>
-          <h1> LOGIN</h1>
-          <div className='inputs'>
-            <label>Email:</label>
-            <input type="email" name="email" />
+      <Header />
+      <main className='render'>
+        <section className='form-section'>
+          <div className='form-div'>
+            <div className='form'>
+              <div className='image'>
+                <img src={cat} alt='' loading='lazy' />
+              </div>
+              <form onSubmit={handleSubmit} className='auth'>
+                <h1> LOGIN</h1>
+                <div className='inputs'>
+                  <label>Email:</label>
+                  <input type="email" name="email" />
+                </div>
+                <div className='inputs'>
+                  <label>Password:</label>
+                  <input type="password" name="password" />
+                </div>
+                <button type="submit" className='submit' disabled={isLoading}>Submit</button>
+                <Link to='/register' className='link-dom'> Don't have an account? <strong> Sign up</strong></Link>
+              </form>
+            </div>
           </div>
-          <div className='inputs'>
-            <label>Password:</label>
-            <input type="password" name="password" />
-          </div>
-          <button type="submit" className='submit' disabled={isLoading}>Submit</button>
-          <Link to='/register' className='link-dom'> Don't have an account? <strong> Sign up</strong></Link>
-        </form>
-        </div>
-        </div>
         </section>
-        
-    </main>
-    <Footer />
+      </main>
+      <Footer />
     </>
   );
 };
