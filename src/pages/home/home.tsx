6cb5004ab0da82759ee2ee06b02f9d8eac08f5c3
vivalaclara <claramaestri07@gmail.com>
2024-02-19 00:26:59 -0300

    about section structure and styles

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,17 +1,18 @@
 import Header from "../../components/header/header"
 import Footer from "../../components/footer/footer"
 import cat from '../../assets/readercat.webp'
+import img from '../../assets/sleepycat.webp'
 import './home.css';
 
 export default function Home(){
     return(
         <>
         <Header />
-        <main> 
+        <main className="render"> 
             <section className="intro">
                 <div className="intro-container">
                 <div className="info">
-                    <h1>CAT TALES</h1>
+                    <h1>TECH-CAT BOOKSTORE</h1>
                     <h2> <em> purrrrfect </em> technology books just for <strong> you </strong></h2>
                     <p>
                         
@@ -19,13 +20,30 @@ export default function Home(){
                         Owned by a tech-savvy feline, our bookstore offers a unique selection of books to satisfy your inner geek,
                         all curated with a paw-some attention to detail.
                     </p>
+                    <a href="#about"><button className="intro-btn">KNOW MORE</button></a>
                 </div>
                 
                     <img src={cat} alt='a ginger cat and a woman on a computer surrounded by books'/>
                 
                 </div>
             </section>
-            <h1> aqui vai ser a home - /login é a rota de login e /register a de cadastro</h1></main>
+            <section id="about" className="about">
+                <div className="about-container">
+                <img src={img} alt='a ginger cat sleeping on a book beside a computer'/>
+                <div className="about-info">
+                <h1>TECH-CAT: the technology bookstore</h1>
+                <h2> learn with our tech books!</h2>
+                <p>
+                    At Tech-Cat, our curated collection of coding and technology books caters to all skill levels. 
+                    From beginner-friendly introductions to advanced topics like machine learning and blockchain, 
+                    our feline owner ensures you have the resources to succeed. Dive into clear explanations and practical examples, 
+                    taking steps towards mastering the digital world with Tech-Cat as your guide.
+
+                </p>
+                </div>
+                </div>
+            </section>
+           </main>
         <Footer />
         </>
     )
