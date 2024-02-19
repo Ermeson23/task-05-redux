f973c1a3d5688f9f61b188a91ae042d1a6494e6f
unknown <Ermeson23>
2024-02-18 22:25:38 -0300

    Fix: Updating bookList page

--- src/pages/bookList/BookList.tsx
+++ src/pages/bookList/BookList.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { useGetBookQuery } from '../../store/api/book';
+import { BookData, useGetBookQuery } from '../../store/api/book';
+import { addItem } from '../../store/api/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import Header from '../../components/header/header';
 
 const BookList: React.FC = () => {
   const { data, isLoading } = useGetBookQuery();
+
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+
+  console.log("Estado inicial do carrinho:", cartItems);
+
+  const handleAddToCart = (product: BookData) => {
+    dispatch(addItem({ product: product, quantity: 1 }));
+    alert("Adicionado ao carrinho com sucesso.");
+    console.log("Estado atual do carrinho:", cartItems);
+  };
 
   if (isLoading) {
     return <p>Carregando livros...</p>;
@@ -16,6 +31,7 @@ const BookList: React.FC = () => {
 
   return (
     <div>
+      <Header />
       <h2>Book List</h2>
       <ul>
         {books.map((book) => (
@@ -25,7 +41,7 @@ const BookList: React.FC = () => {
               <h3>{book.title}</h3>
               <p>{book.subtitle ? book.subtitle : '(No subtitle available)'}</p>
               <p>{book.price}</p>
-              <button>Adicionar ao carrinho</button>
+              <button onClick={() => handleAddToCart(book.isbn13)}>Adicionar ao carrinho</button>
               <a href={book.url}>More Info</a>
             </div>
           </li>
