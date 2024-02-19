1a55594f12b313b902f5386e8de1e2de44049971
vivalaclara <claramaestri07@gmail.com>
2024-02-19 03:28:10 -0300

    booklist styles

--- src/pages/bookList/BookList.tsx
+++ src/pages/bookList/BookList.tsx
@@ -1,6 +1,9 @@
 import { BookData, useGetBookQuery } from '../../store/api/book';
 import { addItem } from '../../store/api/cartSlice';
 import { useDispatch } from 'react-redux';
+import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
+import './booklist.css'
 
 export default function BookList() {
   const { data, isLoading } = useGetBookQuery();
@@ -22,22 +25,36 @@ export default function BookList() {
   const books: BookData[] = data.books as BookData[];
 
   return (
-    <div>
-      <h2>Book List</h2>
-      <div>
-        {books.map((book: BookData) => (
-          <div key={book.isbn13}>
-            <img src={book.image} alt={book.title} />
-            <div>
-              <h3>{book.title}</h3>
-              <p>{book.subTitle ? book.subTitle : '(No subtitle available)'}</p>
-              <p>{book.price}</p>
-              <button onClick={() => handleAddToCart(book)}>Add to cart</button>
-              <a href={book.url}>More Info</a>
+    <>
+    <Header />
+    <main className='render'>
+      <section className='shop-section'>
+       <div className='shop-intro'>
+        <h1> OUR BOOK SELECTION</h1>
+        <p> 
+            Tech-Cat's catalog offers a paw-some array of coding and tech books, carefully curated to satisfy every curiosity.
+            From beginner primers to advanced guides, embark on your tech journey with our purr-fect selection.
+        </p>
+        </div>
+        <h2>Book List</h2>
+        <div className='shop-container'>
+          {books.map((book: BookData) => (
+            <div  className='book-card' key={book.isbn13}>
+              <img src={book.image} alt={book.title} loading='lazy' />
+                <h3>{book.title}</h3>
+                <p>{book.subTitle ? book.subTitle : '(No subtitle available)'}</p>
+                <p>{book.price}</p>
+                <button onClick={() => handleAddToCart(book)}>  Add to cart</button>
+                
+             
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
+          ))}
+        </div>
+
+    </section>
+
+    </main>
+    <Footer />
+    </>
   );
 };
